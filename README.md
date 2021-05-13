064b520776db08c9748464c79c4b4329be3c95d5
Tony Finch <dot@dotat.at>
2021-05-13 23:38:42 +0100

    readme

---
+++ README.md
@@ -0,0 +1,6 @@
+Rust version of my DNS-specific qp-trie
+=======================================
+
+experimental work-in-progress
+
+_Tony Finch `<dot@dotat.at>`_
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ fn main() {
     for byte in 0..=255 {
         let bits = dname::BYTE_TO_BITS[byte];
         if bits < 256 {
-            print!("   {:02x}", bits);
+            print!("  {:02x}", bits);
         } else {
-            print!(" {:04x}", bits);
+            print!("{:04x}", bits);
         }
-        if byte & 7 == 7 {
+        if byte & 15 == 15 {
             println!();
+        } else {
+            print!(" ")
         }
     }
 }
