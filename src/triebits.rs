89f57d096c00f9eef2276b760a1009309104bc64
Tony Finch <dot@dotat.at>
2021-05-22 23:27:39 +0100

    fix triebits test and preen

--- src/triebits.rs
+++ src/triebits.rs
@@ -26,10 +26,9 @@ const fn gen_byte_to_bits() -> [u16; 256] {
     let mut bit_two = SHIFT_BITMAP;
     let mut escaping = true;
     let mut table = [0u16; 256];
-    let mut byte = 0;
-    loop {
-        let i = byte as usize;
-        match byte {
+    let mut i = 0;
+    while i < 256 {
+        match i as u8 {
             // common characters
             b'-'..=b'9' | b'_'..=b'z' => {
                 escaping = false;
@@ -37,7 +36,7 @@ const fn gen_byte_to_bits() -> [u16; 256] {
                 table[i] = bit_one as u16;
             }
             // map upper case to lower case
-            b'A'..=b'Z' => {
+            byte @ b'A'..=b'Z' => {
                 table[i] = (
                     (bit_one + 1) + // bump past escape character
                      (b'a' - b'_') + // and skip non-letters
@@ -56,12 +55,9 @@ const fn gen_byte_to_bits() -> [u16; 256] {
                 bit_two += 1;
             }
         }
-        if byte == 255 {
-            return table;
-        } else {
-            byte += 1;
-        }
+        i += 1;
     }
+    table
 }
 
 #[cfg(test)]
@@ -69,7 +65,6 @@ mod test {
     use super::*;
 
     #[test]
-    #[should_panic]
     fn byte_to_bits() {
         for byte in b'a'..=b'z' {
             let lower = byte as usize;
@@ -81,16 +76,16 @@ mod test {
             let hi = (BYTE_TO_BITS[i] >> 8) as u8;
             assert!(lo >= SHIFT_BITMAP);
             assert!(lo < SHIFT_OFFSET);
-            assert!(hi >= SHIFT_BITMAP);
+            assert!(hi >= SHIFT_BITMAP || hi == 0);
             assert!(hi < SHIFT_OFFSET);
         }
         for i in 0..=254 {
             let j = i + 1;
-            let ilo = (BYTE_TO_BITS[i] & 0xFF) as u8;
-            let ihi = (BYTE_TO_BITS[i] >> 8) as u8;
-            let jlo = (BYTE_TO_BITS[j] & 0xFF) as u8;
-            let jhi = (BYTE_TO_BITS[j] >> 8) as u8;
-            assert!(ilo <= jlo);
+            let ilo = BYTE_TO_BITS[i] & 0xFF;
+            let ihi = BYTE_TO_BITS[i] >> 8;
+            let jlo = BYTE_TO_BITS[j] & 0xFF;
+            let jhi = BYTE_TO_BITS[j] >> 8;
+            assert!(ilo <= jlo || i as u8 == b'Z');
             if ilo == jlo {
                 assert!(ihi < jhi);
             }
